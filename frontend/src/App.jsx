013c322d06e069d3f7cbe645e92548994e22aa1e
BabcsanyP95 <petibabcsany@gmail.com>
2026-09-24 19:06:48 +0200

    add backend deploy URL to frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react"
 import axios from "axios"
 import Chart from "./Chart"
 
-const API = "https://budget-planner-api-tojd.onrender.com/"
+const API = "https://budget-planner-api-tojd.onrender.com"
 
 function App() {
   const [transactions, setTransactions] = useState([])
